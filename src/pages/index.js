d5c7630e2ccea47d91576bf483ffa9d13ee7acc4
Ricardo Ruiz <gaanzz11@gmail.com>
2020-07-29 21:00:31 -0500

    Resolved map problem on webpack build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import { Helmet } from "react-helmet"
 
 import Layout from '../components/layout';
 import Hashtags from './Hashtags'
-import IndexPosts from './IndexPosts';
+import './Index.css';
 
 
 const IndexPage = ({ data }) => {
@@ -14,10 +14,37 @@ const IndexPage = ({ data }) => {
 		<Layout>
 			<Helmet title={"Home | The Art of Design"} />
 			<Hashtags />
-			<IndexPosts props={edges}/>
+			<div id="posts">
+				<h3>También puedes revisar los últimos artículos:</h3>
+				{edges.map((edge, index) => {
+					const { frontmatter } = edge.node;
+					return (
+						<div  key={index}>
+						<Link to={frontmatter.path}                 
+							className="post-link">
+								<div className="post">
+										<div>
+			
+												<h3 className="post-title">{frontmatter.title}</h3>
+												<em className="post-date">{frontmatter.date}</em>                            
+												<br/>
+												<div className="post-tag">
+													{frontmatter.tags.map((tag) => "#" + tag + " ")}
+												</div>
+												
+										</div>
+								</div>
+						</Link>
+						<hr/>
+					</div>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 };
+
+
 
 export const query = graphql`
 	query HomePageQuery {
