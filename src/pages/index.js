904103af976ef9674bb0a20e92042d81c62ec992
Ricardo Ruiz <gaanzz11@gmail.com>
2020-08-09 15:39:41 -0500

    Home page changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,71 +1,31 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
-import Img from "gatsby-image"
+import { graphql} from "gatsby"
+
+import StyledBackgroundSection from "./background"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Hashtags from "./Hashtags"
-import Search from "../components/search"
-
-import { Posts, PostCard } from "../styles/styles"
 
 const IndexPage = ({ data }) => {
-  const { edges } = data.allMarkdownRemark
 
   return (
-    <Layout>
-      <SEO title="The Art of design" description="A design articles site." />
-      <Search searchIndex={data.siteSearchIndex.index} />
-      <Hashtags />
-      <Posts>
-        <h3>También puedes revisar los últimos artículos:</h3>
-        {edges.map((edge, index) => {
-          const { frontmatter } = edge.node
-          return (
-            <PostCard>
-              <Link to={frontmatter.path + "#topp"} key={index}>
-                <Img
-                  fixed={frontmatter.srcPath.childImageSharp.fixed}
-                  alt="A corgi smiling happily"
-                />
-                <h3>{frontmatter.title}</h3>
-                <h4>
-                  <small>
-                    <em>{frontmatter.date}</em>
-                  </small>
-                </h4>
-                <p>{frontmatter.tags.map((tag) => "#" + tag + " ")}</p>
-              </Link>
-            </PostCard>
-          )
-        })}
-      </Posts>
-    </Layout>
+      <Layout>
+        <SEO title="The Art of design" description="A design articles site." />
+        <StyledBackgroundSection url="/tags" image={0} title={"#Tags"}/>
+        <StyledBackgroundSection url="/everything" image={1} title={"#Everything"}/>
+        <StyledBackgroundSection url="/buscar" image={2} title={"#Buscar"} />
+      </Layout>
   )
 }
 
 export const query = graphql`
   query HomePageQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }, limit: 4) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "MMMM DD, YYYY")
-            path
-            tags
-            excerpt
-            srcPath {
-              childImageSharp {
-                fixed(width: 250, height: 200) {
-                  ...GatsbyImageSharpFixed
-                }
-              }
-            }
-          }
-        }
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        siteUrl
       }
     }
     siteSearchIndex {
