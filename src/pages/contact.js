fda3965a5d3cde5691da1bdc5d08663ce35c7c79
Ricardo Ruiz <gaanzz11@gmail.com>
2020-08-10 00:46:28 -0500

    Form page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,8 @@ const Contact = () => {
         <Layout>
             <TagsStyle>
             <h3>Contacto: </h3>
-            <form name="contact" method="POST" data-netlify="true">
+            <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+                <input type="hidden" name="form-name" value="contact"/>
                 <p>
                     <label htmlFor="name">Nombre: <br/> <input type="text" name="name" /></label>   
                 </p>
